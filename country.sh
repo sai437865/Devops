7880b593a90f1d144242bce4b85b7976272f763a
saikumargama <saikumargama99@gmail.com>
2024-12-30 17:56:52 +0530

    new lines added

--- country.sh
+++ country.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Copilot Edits is a multi-file editing experience that you can open from the top of the chat side bar. Given a prompt, Edits will propose changes across files including creating new files when needed. This gives you the conversational flow of chat combined with the power of Copilot's code generation capabilities. The result is something you have to try to believe."
+
+echo "Rita Hey Tina? Is it you?"
+
+echo "Tina Oh Rita! How are you? It’s been a long time."
+
